76e7d4b5ca5dc99238735208682a8bbeb2f4b7ef
feerolink-creator <feerolink-creator@outlook.com>
2025-06-27 19:24:00 +0900

    Update script.js

--- relationship-viewer_JP/script.js
+++ relationship-viewer_JP/script.js
@@ -140,7 +140,8 @@ nodes.forEach((node) => {
   // ノード
   if (node.burden < -8) {
     const image = document.createElementNS("http://www.w3.org/2000/svg", "image");
-    image.setAttributeNS("http://www.w3.org/1999/xlink", "href", "images/funa.PNG");
+    image.setAttributeNS("http://www.w3.org/1999/xlink", "href", "relationship-viewer_JP/
+images/funa.png");
     image.setAttribute("x", x - 20);
     image.setAttribute("y", y - 20);
     image.setAttribute("width", "40");
@@ -203,7 +204,8 @@ document.getElementById("addNodeBtn").addEventListener("click", () => {
 
   if (burden < -8) {
     const image = document.createElementNS("http://www.w3.org/2000/svg", "image");
-    image.setAttributeNS("http://www.w3.org/1999/xlink", "href", "images/funa.PNG");
+    image.setAttributeNS("http://www.w3.org/1999/xlink", "href", "relationship-viewer_JP/
+images/funa.png");
     image.setAttribute("x", x - 20);
     image.setAttribute("y", y - 20);
     image.setAttribute("width", "40");
